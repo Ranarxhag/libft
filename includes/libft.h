107f3f223555c98f1be3810f52dad9692b14e7d0
Ranarxhag <boris.yova@gmail.com>
2019-02-21 22:06:22 +0100

    Add str3join.c declaration in libft.h and Makefile

--- includes/libft.h
+++ includes/libft.h
@@ -70,6 +70,8 @@ int					ft_strequ(char const *s1, char const *s2);
 int					ft_strnequ(char const *s1, char const *s2, size_t n);
 char				*ft_strsub(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
+char				*ft_str3join(char const *s1, char const *s2,
+							char const *s3);
 char				*ft_strtrim(char const *s);
 char				**ft_strsplit(char const *s, char c);
 char				*ft_itoa(int n);
